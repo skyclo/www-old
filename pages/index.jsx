b3ed4e408b68cb4fc56d0ebbb35aa0b6a176df3a
Conor Daly <conor.daly@ucf.edu>
2021-10-20 17:12:10 -0400

    🚧 wip: set up homepage

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Skyclo | Home</title>
+            </Head>
+        </>
+    );
+}
