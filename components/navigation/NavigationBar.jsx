44c78ca3202418cefb351fcfa825c991ce478eaa
Conor Daly <conor.daly@ucf.edu>
2021-10-27 13:39:58 -0400

    🍱 feat: updated brand icon to pure white transparent svg

--- components/navigation/NavigationBar.jsx
+++ components/navigation/NavigationBar.jsx
@@ -6,7 +6,7 @@ export default function NavigationBar() {
         <nav className="flex flex-row w-full h-12 py-2 px-8 bg-transparent">
             <Link href="/">
                 <a>
-                    <img src="/images/Skyclo_logo_460x460.png" className="w-6 h-6 my-auto" alt="Brand Logo"/>
+                    <img src="/images/Skyclo_logo.svg" className="w-6 h-6 my-auto" alt="Brand Logo"/>
                 </a>
             </Link>
             <div className="ml-auto my-auto">
