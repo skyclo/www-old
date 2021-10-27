e7a499a0936462590e0690b0f61a917632520310
Conor Daly <conor.daly@ucf.edu>
2021-10-27 15:23:05 -0400

    📱 fix: improve responsive design

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,10 +1,10 @@
 
 export default function Header({headerName, colorFrom, colorTo}) {
     return (
-        <header className="w-full h-1/2 -mt-12">
-            <div className="flex flex-col w-full h-full bg-gray-950">
-                <div className="my-auto max-w-7xl w-full mx-auto">
-                    <h1 className={"w-min font-sans font-extrabold text-5xl text-left tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-" + colorFrom + " to-" + colorTo}>
+        <header className="w-full h-1/4 md:h-1/3 lg:h-1/2 -mt-12">
+            <div className="flex flex-col w-full h-full px-4 lg:px-12 xl:px-24 bg-gray-950">
+                <div className="my-auto w-full mx-auto">
+                    <h1 className={"mt-12 mx-auto lg:mx-0 w-min font-sans font-extrabold text-3xl md:text-4xl lg:text-5xl text-center lg:text-left tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-" + colorFrom + " to-" + colorTo}>
                         {headerName}
                     </h1>
                 </div>
