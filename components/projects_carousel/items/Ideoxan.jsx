a18e7a1dc3a853a2b5b381df3bd44dbb4e5dab94
Conor Daly <conor.daly@ucf.edu>
2021-10-29 17:12:26 -0400

    💄 fix: fix responsiveness issues

--- components/projects_carousel/items/Ideoxan.jsx
+++ components/projects_carousel/items/Ideoxan.jsx
@@ -7,7 +7,7 @@ export default function IdeoxanProjectCarousel () {
                     <img src="/images/beta_logo1_500px_accent_nobg.png" alt="Ideoxan Logo" className="w-5 h-auto my-auto"/>
                     <h3 className="my-auto ml-2 font-sans text-gray-50 font-bold text-3xl">ideoxan</h3>
                 </div>
-                <p className="mt-2 mx-auto lg:mx-0 font-sans text-gray-200 font-medium text-normal text-center lg:text-left">Empowering the next generation of programmers, makers, and inventors.</p>
+                <p className="mt-2 mx-auto lg:mx-0 pr-0 lg:pr-2 font-sans text-gray-200 font-medium text-normal text-center lg:text-left">Empowering the next generation of programmers, makers, and inventors.</p>
                 <a href="https://ideoxan.com" className="mt-4 mx-auto lg:mx-0 px-8 py-3 bg-purple-600 w-max rounded-md text-sans text-purple-100 font-semibold text-sm shadow-sm">
                     Learn More
                 </a>
