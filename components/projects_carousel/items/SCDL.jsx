5d1ccba695786a5e65b5ef393e6a370548149649
Conor Daly <conor.daly@ucf.edu>
2021-11-01 14:31:34 -0400

    💄 fix: fix button styling

--- components/projects_carousel/items/SCDL.jsx
+++ components/projects_carousel/items/SCDL.jsx
@@ -19,7 +19,7 @@ export default function SCDLProjectCarousel () {
                     <h3 className="my-auto font-sans text-gray-50 font-bold text-3xl">node-scdl</h3>
                 </div>
                 <p className="mt-2 mx-auto lg:mx-0 pr-0 lg:pr-2 font-sans text-gray-200 font-medium text-normal text-center lg:text-left">An easy to use, minimal dependency Soundcloud song downloader package for NodeJS.</p>
-                <a href="https://github.com/skyclo/node-scdl" className="mt-4 mx-auto lg:mx-0 px-8 py-3 bg-blue-500 w-max rounded-md text-sans text-blue-100 font-semibold text-sm shadow-sm">
+                <a href="https://github.com/skyclo/node-scdl" className="mt-4 mx-auto lg:mx-0 px-8 py-3 bg-blue-500 w-max rounded-md text-sans text-blue-100 font-semibold text-sm shadow-sm ring-1 ring-blue-50 ring-opacity-20">
                     View the Source Code
                 </a>
             </div>
