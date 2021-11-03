ba3caf790e518f9dc91c0af4e0edb4816670af5e
Conor Daly <conor.daly@ucf.edu>
2021-11-03 17:15:47 -0400

    🐛 fix: fix discord embed properties

--- components/meta/Meta.jsx
+++ components/meta/Meta.jsx
@@ -12,24 +12,23 @@ export default function HeadMetaTags ({pageTitle}) {
 
             <meta name="title" content={"Conor's Website" + pageTitle}/>
             <meta name="description" content="Conor's interactive, online portfolio."/>
+
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content="https://skyclo.dev/"/>
+            <meta property="og:title" content={"Conor's Website" + pageTitle}/>
+            <meta property="og:description" content="Conor's interactive, online portfolio."/>
+            <meta property="og:image" content="https://skyclo.dev/images/og_thumbnail.png"/>
+
             <meta name="keywords" content="Skyclo, Conor Daly, Bryce Daly, Programming, Portfolio, ePortfolio, Online Portfolio, Website Development, Web Dev, Coding, Ideoxan, Serenade, node-scdl, Developer Portfolio, Developer, Backend, Frontend, Resume"/>
             <meta name="image" content="https://skyclo.dev/images/og_thumbnail.png"/>
             <meta name="theme-color" content="#050a15"/>
 
             <meta name="twitter:card" content="summary_large_image"/>
-            <meta name="twitter:site" content="@skyclo_"/>
-            <meta name="twitter:creator" content="@skyclo_"/>
+            <meta name="twitter:url" content="https://skyclo.dev/"/>
             <meta name="twitter:title" content={"Conor's Website" + pageTitle}/>
+            <meta name="twitter:description" content="Conor's interactive, online portfolio."/>
             <meta name="twitter:image" content="https://skyclo.dev/images/og_thumbnail.png"/>
-            <meta name="twitter:url" content="https://skyclo.dev"/>
 
-            <meta prefix="og: http://ogp.me/ns#" property="og:title" content={"Conor's Website" + pageTitle}/>
-            <meta prefix="og: http://ogp.me/ns#" property="og:type" content="photo"/>
-            <meta prefix="og: http://ogp.me/ns#" property="og:locale" content="en_US"/>
-            <meta prefix="og: http://ogp.me/ns#" property="og:site_name" content="Skyclo"/>
-            <meta prefix="og: http://ogp.me/ns#" property="og:url" content="https://skyclo.dev/"/>
-            <meta prefix="og: http://ogp.me/ns#" property="og:image" content="https://skyclo.dev/images/og_thumbnail.png"/>
-            <meta prefix="og: http://ogp.me/ns#" name="theme-color" content="#050a15"/>
         </Head>
     )
 }
