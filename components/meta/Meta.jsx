127e5449e60af32ce7e4a85a41efb17a43f6f536
Conor Daly <conor.daly@ucf.edu>
2021-11-03 16:51:49 -0400

    🩹 fix: fix broken links

--- components/meta/Meta.jsx
+++ components/meta/Meta.jsx
@@ -13,14 +13,14 @@ export default function HeadMetaTags ({pageTitle}) {
             <meta name="title" content={"Conor's Website" + pageTitle}/>
             <meta name="description" content="Conor's interactive, online portfolio."/>
             <meta name="keywords" content="Skyclo, Conor Daly, Bryce Daly, Programming, Portfolio, ePortfolio, Online Portfolio, Website Development, Web Dev, Coding, Ideoxan, Serenade, node-scdl, Developer Portfolio, Developer, Backend, Frontend, Resume"/>
-            <meta name="image" content="/images/og_thumbnail.png"/>
+            <meta name="image" content="https://skyclo.dev/images/og_thumbnail.png"/>
             <meta name="theme-color" content="#050a15"/>
 
             <meta name="twitter:card" content="summary_large_image"/>
             <meta name="twitter:site" content="@skyclo_"/>
             <meta name="twitter:creator" content="@skyclo_"/>
             <meta name="twitter:title" content={"Conor's Website" + pageTitle}/>
-            <meta name="twitter:image" content="/images/og_thumbnail.png"/>
+            <meta name="twitter:image" content="https://skyclo.dev/images/og_thumbnail.png"/>
             <meta name="twitter:url" content="https://skyclo.dev"/>
 
             <meta prefix="og: http://ogp.me/ns#" property="og:title" content={"Conor's Website" + pageTitle}/>
@@ -29,7 +29,7 @@ export default function HeadMetaTags ({pageTitle}) {
             <meta prefix="og: http://ogp.me/ns#" property="og:site_name" content="Skyclo"/>
             <meta prefix="og: http://ogp.me/ns#" property="og:description" content="Conor's interactive, online portfolio."/>
             <meta prefix="og: http://ogp.me/ns#" property="og:url" content="https://skyclo.dev/"/>
-            <meta prefix="og: http://ogp.me/ns#" property="og:image" content="/images/og_thumbnail.png"/>
+            <meta prefix="og: http://ogp.me/ns#" property="og:image" content="https://skyclo.dev/images/og_thumbnail.png"/>
             <meta prefix="og: http://ogp.me/ns#" name="theme-color" content="#050a15"/>
         </Head>
     )
