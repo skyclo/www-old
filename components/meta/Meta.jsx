2e911f18b4ef2d0900fa7f2b84b3a37362bd79b4
Conor Daly <conor.daly@ucf.edu>
2021-11-03 17:22:41 -0400

    🐛 fix: fix discord embed properties

--- components/meta/Meta.jsx
+++ components/meta/Meta.jsx
@@ -8,26 +8,29 @@ export default function HeadMetaTags ({pageTitle}) {
             <meta charSet="UTF-8"/>
             <meta lang="en"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <title>Skyclo{pageTitle}</title>
 
+            {/* Primary Meta Tags */}
+            <title>Skyclo{pageTitle}</title>
             <meta name="title" content={"Conor's Website" + pageTitle}/>
             <meta name="description" content="Conor's interactive, online portfolio."/>
 
+            {/* Open Graph / Facebook */}
             <meta property="og:type" content="website"/>
-            <meta property="og:url" content="https://skyclo.dev/"/>
+            <meta property="og:url" content="https://www.skyclo.dev/"/>
             <meta property="og:title" content={"Conor's Website" + pageTitle}/>
             <meta property="og:description" content="Conor's interactive, online portfolio."/>
-            <meta property="og:image" content="https://skyclo.dev/images/og_thumbnail.png"/>
+            <meta property="og:image" content="https://www.skyclo.dev/images/og_thumbnail.png"/>
+
+            {/* Twitter */}
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta property="twitter:url" content="https://www.skyclo.dev/"/>
+            <meta property="twitter:title" content={"Conor's Website" + pageTitle}/>
+            <meta property="twitter:description" content="Conor's interactive, online portfolio."/>
+            <meta property="twitter:image" content="https://www.skyclo.dev/images/og_thumbnail.png"/>
 
             <meta name="keywords" content="Skyclo, Conor Daly, Bryce Daly, Programming, Portfolio, ePortfolio, Online Portfolio, Website Development, Web Dev, Coding, Ideoxan, Serenade, node-scdl, Developer Portfolio, Developer, Backend, Frontend, Resume"/>
-            <meta name="image" content="https://skyclo.dev/images/og_thumbnail.png"/>
+            <meta name="image" content="https://www.skyclo.dev/images/og_thumbnail.png"/>
             <meta name="theme-color" content="#050a15"/>
-
-            <meta name="twitter:card" content="summary_large_image"/>
-            <meta name="twitter:url" content="https://skyclo.dev/"/>
-            <meta name="twitter:title" content={"Conor's Website" + pageTitle}/>
-            <meta name="twitter:description" content="Conor's interactive, online portfolio."/>
-            <meta name="twitter:image" content="https://skyclo.dev/images/og_thumbnail.png"/>
 
         </Head>
     )
