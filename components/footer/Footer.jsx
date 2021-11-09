47678dc7b966fe12b6ea415973b6a8cba0a71758
Conor Daly <conor.daly@ucf.edu>
2021-11-09 14:06:56 -0500

    💫 fix: add animations to footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Footer () {
     return (
         <footer className="relative flex flex-row w-full mt-16 bg-gray-950 overflow-hidden">
-            <div className="px-12 py-14">
+            <div className="animate-fadeupwards px-12 py-14">
                 <h2 className="font-sans font-extrabold tracking-wide text-left text-4xl text-gray-50 opacity-30">skyclo</h2>
                 <h3 className="mt-0.5 font-sans font-bold tracking-normal text-left text-xs text-gray-50 opacity-30">A Conor Daly Brand</h3>
                 <p className="mt-6 font-sans font-medium text-left text-xs text-gray-50 opacity-30">(C) 2018 - 2021</p>
