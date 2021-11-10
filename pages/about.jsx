a9020b0ba4da4445f06bd2f2ef394a668db0060c
Conor Daly <conor.daly@ucf.edu>
2021-11-10 15:42:52 -0500

    ✨ feat: add awards section, add courses section, add certifications section, add clubs section

--- pages/about.jsx
+++ pages/about.jsx
@@ -52,7 +52,7 @@ export default function About() {
             rankName: 'Intermediate',
         },
         {
-            skillName: 'React',
+            skillName: 'ReactJS and Next.js',
             stars: 2,
             rankName: 'Beginner',
         },
@@ -96,6 +96,69 @@ export default function About() {
                         }
                     </div>
                 </div>
+                <div className="grid grid-flow-row auto-cols-fr auto-rows-min grid-cols-1 lg:grid-cols-2 lg:grid-flow-col w-full px-24 py-16 gap-x-0 lg:gap-x-24 gap-y-32 lg:gap-y-0">
+                    <div class="flex flex-col w-full space-y-16">
+                        <div class="flex flex-col w-full">
+                            <h2 className="animate-fadeupwards mx-auto lg:mx-0 font-sans font-extrabold text-center lg:text-left text-4xl tracking-tight text-gray-950">Relevant Coursework</h2>
+                            <div className="animate-fadeupwards flex flex-col mt-8 mx-auto lg:ml-4 lg:mr-0 font-sans font-normal text-center lg:text-left text-gray-700">
+                                <ul className="list-disc space-y-2">
+                                    <li>Intro to Programming with C</li>
+                                    <li>Calculus III</li>
+                                    <li>AP Calculus AB and BC</li>
+                                    <li>Digital Graphical Art 1 and 2</li>
+                                    <li>AP Computer Science Principles and A</li>
+                                    <li>Modeling and Simulation I-IV</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="flex flex-col w-full">
+                            <h2 className="animate-fadeupwards mx-auto lg:mx-0 font-sans font-extrabold text-center lg:text-left text-4xl tracking-tight text-gray-950">Clubs and Involvement</h2>
+                            <div className="animate-fadeupwards flex flex-col mt-8 mx-auto lg:ml-4 lg:mr-0 font-sans font-normal text-center lg:text-left text-gray-700">
+                                <ul className="list-disc space-y-2">
+                                    <li>Knight Hacks 2021 Project Team 12</li>
+                                    <li>100+ Hours of Community Service in High School</li>
+                                    <li>Modeling and Simulation Club ("Mod Squad")</li>
+                                    <li>FIRST Technical Challenge Robotics Club</li>
+                                    <li>Student Government Association</li>
+                                    <li>Business Professionals of America</li>
+                                    <li>Science National Honors Society</li>
+                                    <li>Key Club</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col w-full space-y-16">
+                    <div class="flex flex-col w-full">
+                            <h2 className="animate-fadeupwards mx-auto lg:mx-0 font-sans font-extrabold text-center lg:text-left text-4xl tracking-tight text-gray-950">Certifications</h2>
+                            <div className="animate-fadeupwards flex flex-col mt-8 mx-auto lg:ml-4 lg:mr-0 font-sans font-normal text-center lg:text-left text-gray-700">
+                                <ul className="list-disc space-y-2">
+                                    <li>NCS-100 Modeling and Simulation Certification</li>
+                                    <li>Adobe Certified Associate</li>
+                                    <li>IC3 Digital Literacy Certification (GS4)</li>
+                                    <li>IC3 Microsoft Office 365 Certified Associate</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="flex flex-col w-full">
+                            <h2 className="animate-fadeupwards mx-auto lg:mx-0 font-sans font-extrabold text-center lg:text-left text-4xl tracking-tight text-gray-950">Awards and Recognition</h2>
+                            <div className="animate-fadeupwards flex flex-col mt-8 mx-auto lg:ml-4 lg:mr-0 font-sans font-normal text-center lg:text-left text-gray-700">
+                                <ul className="list-disc space-y-2">
+                                    <li>2021 AP Scholar with Distinction</li>
+                                    <li>2021 Florida BrightFutures Academic Scholar</li>
+                                    <li>Business Professionals of America, 2021 Regional Graphic Design Competition, 5th Place</li>
+                                    <li>National Scholastic Press Association, 2020 Best in Show Award, 5th Place</li>
+                                    <li>2020 AP Scholar with Distinction</li>
+                                    <li>National Center for Simulation, 2020 Team Orlando Day Competition, 4th Place</li>
+                                    <li>FIRST Robotics, 2018 World Championship, FIRST Technical Challenge, Design Award</li>
+                                    <li>FIRST Robotics, 2018 Florida Championship, FIRST Technical Challenge, Inspire Award</li>
+                                    <li>FIRST Robotics, 2017 Space Coast League Championship, FIRST Technical Challenge, Inspire Award</li>
+                                    <li>2016 United States Department of Education Presidential Award for Academic Excellence</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
                 <Footer/>
             </body>
         </>
