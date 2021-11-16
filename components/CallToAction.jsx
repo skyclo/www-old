a73166648e9357bd791803859b6afae4b50e8af9
Conor Daly <conor.daly@ucf.edu>
2021-11-16 13:01:17 -0500

    🚨 fix linter warnings

--- components/CallToAction.jsx
+++ components/CallToAction.jsx
@@ -10,13 +10,15 @@ export default function CallToAction ({data}) {
                 </div>
                 <div className="animate-fadeupwards flex flex-col sm:flex-row mx-auto lg:mr-0 lg:my-auto sm:space-x-8 ml-auto">
                     {
-                        data.buttons.map((button) => {
+                        data.buttons.map((button, i, arr) => {
                             let IconComponent = Icon[button.icon]
                             return (
                                 <a
                                     target="_blank"
                                     href={button.url}
                                     className="flex flex-row flex-shrink-0 h-min mt-4 mx-auto px-8 py-3 bg-transparent border-2 border-gray-50 w-max rounded-md text-sans text-light font-semibold text-sm"
+                                    key={i}
+                                    rel="noreferrer"
                                 >
                                     <IconComponent className="w-4 h-4 my-auto" strokeLinecap="round" strokeLinejoin="round" fill="none" stroke="currentColor" stroke-width="2"/>
                                     <span className="ml-2 my-auto">{button.text}</span>
