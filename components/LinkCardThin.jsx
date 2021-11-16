0bb9b17999b18c7287a9e6aeeffec58d5b91a60b
Conor Daly <conor.daly@ucf.edu>
2021-11-16 13:03:58 -0500

    🚨 fix linter warnings

--- components/LinkCardThin.jsx
+++ components/LinkCardThin.jsx
@@ -3,7 +3,7 @@ import * as Icon from 'react-feather'
 export default function LinkCardThin({data}) {
     let IconComponent = Icon[data.icon.name]
     return (
-        <a target="_blank" href={data.url} className="flex flex-row flex-grow px-8 py-8 w-full bg-light ring-1 ring-gray-500 ring-opacity-20 shadow-md hover:shadow-lg rounded-lg">
+        <a target="_blank" href={data.url} className="flex flex-row flex-grow px-8 py-8 w-full bg-light ring-1 ring-gray-500 ring-opacity-20 shadow-md hover:shadow-lg rounded-lg" rel="noreferrer">
             <IconComponent
                 className="my-auto w-8 h-auto"
                 strokeLinecap="round"
