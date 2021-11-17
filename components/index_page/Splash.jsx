680a7f6c5f422718f32b57accf91d3c9d44ad2c1
Conor Daly <conor.daly@ucf.edu>
2021-11-17 16:36:22 -0500

    Revert "⚡️ revert refactor splash screen, useEffect in index page""
    This reverts commit 5319e2da7975996ce3a98b4826e11da51c1d25de.

---
+++ components/index_page/Splash.jsx
@@ -0,0 +1,10 @@
+export default function Splash () {
+    return (
+        <div className="absolute flex container-full-screen bg-dark animate-logobackintro opacity-0">
+            <img src="/images/Skyclo_logo.svg" className="my-auto mx-auto w-48 h-auto animate-logointro opacity-0" alt="Skyclo Branding Logo"/>
+            <div className="">
+
+            </div>
+        </div>
+    )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,39 @@
-import Navbar from './../components/navbar/Navbar'
+import { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
-import IntroductionAnimation from './../components/index_page/IntroductionAnimation'
+import Splash from './../components/index_page/Splash'
 import HeadMetadata from './../components/HeadMetadata'
 import DevBanner from '../components/DevBanner'
 
+const Navbar = dynamic(() => import('../components/navbar/Navbar'))
 const IndexPageHero = dynamic(() => import('../components/index_page/IndexPageHero'))
 
 export default function Home() {
+    let [splashVisible, setSplashVisible] = useState(false)
+    useEffect(() => {
+        let key = "sk0_intro_already_shown"
+        let alreadyShown = localStorage.getItem(key)
+        if (!alreadyShown) {
+            setSplashVisible(true)
+            localStorage.setItem(key, true)
+        }
+    }, [])
+
+    if (splashVisible) return (
+        <>
+            <HeadMetadata pageTitle="Home"/>
+            <body className="container-full-screen bg-dark">
+                <Splash/>
+                <DevBanner/>
+                <Navbar/>
+                <IndexPageHero/>
+            </body>
+        </>
+    )
+
     return (
         <>
             <HeadMetadata pageTitle="Home"/>
             <body className="container-full-screen bg-dark">
-                <IntroductionAnimation/>
                 <DevBanner/>
                 <Navbar/>
                 <IndexPageHero/>
