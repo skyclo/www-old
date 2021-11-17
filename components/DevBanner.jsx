be8b15a51cc65b6a689984bce5450cef640c097c
Conor Daly <conor.daly@ucf.edu>
2021-11-17 14:39:25 -0500

    🔧 fix env vars issue

--- components/DevBanner.jsx
+++ components/DevBanner.jsx
@@ -1,7 +1,7 @@
 let packageInfo = require('./../package.json')
 
 export default function DevBanner () {
-    if (process.env.APP_DEV_MODE == 'false') return (<></>)
+    if (process.env.NEXT_PUBLIC_APP_DEV_MODE == 'false') return (<></>)
 
     return (
         <div className="dev-banner-info absolute z-40 top-auto bottom-0 container-full-width py-4 px-3 overflow-hidden">
@@ -9,7 +9,7 @@ export default function DevBanner () {
                 <div className="flex flex-col mr-4 my-auto">
                     <h3 className="animate-fadeupwards font-sans font-extrabold text-left text-sm tracking-tight text-light">You are viewing an in-development version of the website</h3>
                     <p className="animate-fadeupwards mt-0.5 font-sans font-medium text-left text-2xs tracking-normal text-light-body">To view the production-ready build, visit <a href="https://skyclo.dev" className="hover:opacity-70">skyclo.dev</a></p>
-                    <p className="animate-fadeupwards mt-1 font-mono font-normal text-left text-3xs tracking-normal text-light-grayed-out">buildinfo: proj_ver {packageInfo.version}, provider_ver {packageInfo.dependencies['next']}, renderer_ver {packageInfo.dependencies['react']}, deps [{Object.keys(packageInfo.dependencies).join(', ')}], dev_deps [{Object.keys(packageInfo.devDependencies).join(', ')}]</p>
+                    <p className="animate-fadeupwards mt-1 font-mono font-normal text-left text-3xs tracking-normal text-light-grayed-out">buildinfo: proj_ver {packageInfo.version}, provider_ver {packageInfo.dependencies['next']}, renderer_ver {packageInfo.dependencies['react']}, deps [{Object.keys(packageInfo.dependencies).join(', ')}], dev_deps [{Object.keys(packageInfo.devDependencies).join(', ')}], mode {process.env.NEXT_PUBLIC_APP_DEV_MODE}</p>
                 </div>
                 <div
                     className="amimate-fadeupwards ml-auto bg-light my-auto px-4 py-2 rounded-lg text-center text-xs font-sans font-semibold tracking-tight cursor-pointer"
